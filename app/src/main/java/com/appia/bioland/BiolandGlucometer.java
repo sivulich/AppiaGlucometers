ff0ee35b0160f173ebe50c25a61625b2917135a9
Tobías Lifschitz <tlifschitz@itba.edu.ar>
2020-03-23 20:15:53 -0300

    Empty class and refactor

---
+++ app/src/main/java/com/appia/bioland/BiolandGlucometer.java
@@ -0,0 +1,12 @@
+package com.appia.bioland;
+
+import java.util.Date;
+
+public class BiolandGlucometer {
+
+    static public class BiolandMeasurement{
+        byte[] bytes;
+        int value;
+        Date measuredAt;
+    }
+}
