9525dc70a8282a06c8cf7608a2f67e5935de2252
Tobías Lifschitz <tlifschitz@itba.edu.ar>
2020-04-07 00:10:21 -0300

    Comments on BiolandCallbacks

--- app/src/main/java/com/appia/bioland/BiolandCallbacks.java
+++ app/src/main/java/com/appia/bioland/BiolandCallbacks.java
@@ -9,17 +9,29 @@ import java.util.ArrayList;
 
 public interface BiolandCallbacks extends BleManagerCallbacks {
 
+    /**
+     * Called each time the device updates the countdown while performing a measurement.
+     * @param aCount
+     */
     void onCountdownReceived(int aCount);
 
     /**
-     * Called when new measurements are available. This measurements must be stores by the one who
+     * Called when new measurements are available. This measurements must be stored by the one who
      * implements this interface.
      * @param aMeasurements
      */
     void onMeasurementsReceived(ArrayList<BiolandMeasurement> aMeasurements);
 
+    /**
+     * Called when device information is received.
+     * @param aInfo
+     */
     void onDeviceInfoReceived(BiolandInfo aInfo);
 
+    /**
+     * Called when an error occurs during the communication. 
+     * @param aMessage
+     */
     void onProtocolError(String aMessage);
 
 }
